24227ba5b612ee682d7b7cec26d091c1ccb9e08a
Builder <builder@srv.home>
2019-01-03 15:11:18 +0100

    cleanup

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -35,7 +35,6 @@ TARGET_USES_LOGD := true
 #
 # TWRP
 #
-TWHAVE_SELINUX := true
 #TW_USE_TOOLBOX := true
 TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
 TW_CUSTOM_CPU_TEMP_PATH := "/sys/devices/virtual/thermal/thermal_zone1/temp"
@@ -47,16 +46,11 @@ TW_NO_REBOOT_BOOTLOADER := true
 # Filesystem and storage
 TW_INCLUDE_CRYPTO := true
 RECOVERY_SDCARD_ON_DATA := true
-TW_DEFAULT_EXTERNAL_STORAGE := true
-FLASH_FROM_STORAGE := true		# flashes zips from their current location instead of copying them to /tmp
 TW_INCLUDE_NTFS_3G := true
-#TW_NO_EXFAT_FUSE := true
-#TW_NO_EXFAT := true
-#TARGET_USERIMAGES_USE_F2FS := true	# Compatible kernel needed
-#TW_NEVER_UNMOUNT_SYSTEM := true	# Prevent TWRP from unmounting /system
 BOARD_SUPPRESS_SECURE_ERASE := true # "Some MMCs have VERY slow secure erase - use standard erase instead"
 
 # Theme and graphics
 TW_THEME := portrait_mdpi
-#TW_CUSTOM_THEME := device/lenovo/Tab2A710F/recovery/twres
 TW_INCLUDE_FB2PNG := true
+#TW_CUSTOM_THEME := device/lenovo/Tab2A710F/recovery/twres
+
